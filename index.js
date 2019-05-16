4afa596e2ac5717fb630f5c5f64684a17592e3a6
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-05-16 15:24:43 -0700

    Created GET request

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ server.get("/", (req, res) => {
   res.send("Server Testing...");
 });
 
+// endpoints
+server.get("/animals", (req, res) => {
+  const animals = [
+    {
+      id: 1,
+      name: "Bison"
+    },
+    {
+      id: 2,
+      name: "Black Bear"
+    }
+  ];
+  res.status(200).json(animals); // returning json data and correct http status code
+});
+
 server.listen(8000, () => {
   console.log("http://localhost:8000");
 });
